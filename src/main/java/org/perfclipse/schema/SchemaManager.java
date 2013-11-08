4fb72cb8dff787ff45d991db96d0fc7b62ca6a7e
Jakub Knetl <knetl.j@gmail.com>
2013-11-08 17:52:05 +0100

    Add SchemaManager class.
    Class SchemaManager for pulling element declarations from XML Schema.
    
    * Add SchemaManager sketch
    * Add SchemaManger constructor (creates Map of element declarations).
    * Add Unit test for SchemaManager consturctor.
    * Add dependencies in pom.xml for (slf4j, xsom, logback, junit)

---
+++ src/main/java/org/perfclipse/schema/SchemaManager.java
@@ -0,0 +1,129 @@
+/*
+ * Perfclispe
+ * 
+ * 
+ * Copyright (c) 2013 Jakub Knetl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.perfclipse.schema;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+
+import com.sun.xml.xsom.XSContentType;
+import com.sun.xml.xsom.XSElementDecl;
+import com.sun.xml.xsom.XSModelGroup;
+import com.sun.xml.xsom.XSParticle;
+import com.sun.xml.xsom.XSSchemaSet;
+import com.sun.xml.xsom.XSTerm;
+import com.sun.xml.xsom.XSType;
+import com.sun.xml.xsom.parser.XSOMParser;
+
+/**
+ * The SchemaManager class introduces methods for retrieving information
+ * about elements from XML schema.
+ * 
+ * @author Jakub Knetl
+ */
+public class SchemaManager {
+	final static org.slf4j.Logger log = LoggerFactory.getLogger(SchemaManager.class);
+
+	private XSSchemaSet schema;
+	private Map<String, XSElementDecl> elementMap;
+	
+	/**
+	 * Uses XSOM parser to parse XML schema given by URL.
+	 * @param schemaFile location of XML schema
+	 * @throws SAXException
+	 */
+	public SchemaManager(URL schemaFile) throws SAXException {
+		XSOMParser parser = new XSOMParser();
+		try{
+			parser.parse(schemaFile);
+			schema = parser.getResult();
+		} catch (SAXException e){
+			log.error("Cannot parse schema file: " + schemaFile.getPath());
+			throw e;
+		}
+		elementMap = findElements(schema);
+	}
+	
+	/**
+	 * Returns all element names defined by XML schema. 
+	 * 
+	 * @return Set of all element names contained int parsed XML schema.
+	 */
+	public Set<String> getElementNames(){
+		return elementMap.keySet();
+	}
+	
+	/**
+	 * Recursively goes through parsed xml schema and builds
+	 * map<ElementName, ElementType>
+	 * @param schemaSet
+	 * @return Map of element names and type declaration.
+	 */
+	private Map<String, XSElementDecl> findElements(XSSchemaSet schemaSet){
+		Map<String, XSElementDecl> elements = new HashMap<String, XSElementDecl>();
+		Iterator<XSElementDecl> it = schemaSet.iterateElementDecls();
+		while(it.hasNext()){
+			XSElementDecl elementDecl = (XSElementDecl) it.next();
+			XSType type = elementDecl.getType();
+			if (type.isComplexType()){
+				elements.put(elementDecl.getName(), elementDecl);
+				XSContentType contentType = type.asComplexType().getContentType();
+				XSParticle particle = contentType.asParticle();
+				findElements(elements, particle);
+			}
+			
+		}
+		
+		return elements;
+	}
+
+	private void findElements(Map<String, XSElementDecl> elements,
+			XSParticle particle) {
+
+		if (particle == null)
+			return;
+		
+		XSTerm pTerm = particle.getTerm();
+		if (pTerm.isElementDecl()){
+			XSElementDecl elementDecl = pTerm.asElementDecl();
+			XSType type = elementDecl.getType();
+			if (type.isComplexType()){
+				elements.put(elementDecl.getName(), elementDecl);
+				XSContentType contentType = type.asComplexType().getContentType();
+				XSParticle particleChild = contentType.asParticle();
+				findElements(elements, particleChild);
+			}
+		} else if (pTerm.isModelGroup()){
+			XSModelGroup modelGroup = pTerm.asModelGroup();
+			XSParticle[] particleArray = modelGroup.getChildren();
+			for(XSParticle p: particleArray){
+				findElements(elements, p);
+			}
+			
+		}
+	}
+	
+
+}
---
+++ src/test/java/org/perfclipse/schema/SchemaManagerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Perfclispe
+ * 
+ * 
+ * Copyright (c) 2013 Jakub Knetl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.perfclipse.schema;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+
+/**
+ * Class SchemaManagerTest provides unit test for class SchemaManager
+ * 
+ * @author Jakub Knetl
+ *
+ */
+public class SchemaManagerTest {
+	private static final String XSD_PATH = "/simpleSchema.xsd";
+	SchemaManager manager;
+	
+	@Before
+	public void setUp() throws IOException, SAXException{
+		URL xsd = this.getClass().getResource(XSD_PATH);
+		manager = new SchemaManager(xsd);
+	}
+	
+	@Test
+	public void findElements(){
+		Set<String> elementNames = manager.getElementNames();
+
+		Set<String> expectedNames = new HashSet<String>();
+		expectedNames.add("property");
+		expectedNames.add("header");
+		expectedNames.add("scenario");
+		expectedNames.add("properties");
+		expectedNames.add("generator");
+		expectedNames.add("run");
+		expectedNames.add("sender");
+		
+		assertEquals(elementNames, expectedNames);
+	}
+
+
+}
