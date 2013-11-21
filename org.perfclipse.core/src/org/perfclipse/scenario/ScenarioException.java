c240af6f5be25b0982ca11a46fcaf7a5ec32f83d
Jakub Knetl <knetl.j@gmail.com>
2013-11-21 23:00:02 +0100

    Create ScenarioException

---
+++ org.perfclipse.core/src/org/perfclipse/scenario/ScenarioException.java
@@ -0,0 +1,57 @@
+/*
+ * Perfclispe
+ * 
+ * 
+ * Copyright (c) 2013 Jakub Knetl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.perfclipse.scenario;
+
+/**
+ * Exception class which represent error in XML file
+ * which contains scenario according to given XML
+ * schema definiton.
+ * 
+ * @author Jakub Knetl
+ *
+ */
+public class ScenarioException extends Exception {
+
+	public ScenarioException() {
+		// TODO Automaticky generovaný stub konstruktoru
+	}
+
+	public ScenarioException(String message) {
+		super(message);
+		// TODO Automaticky generovaný stub konstruktoru
+	}
+
+	public ScenarioException(Throwable cause) {
+		super(cause);
+		// TODO Automaticky generovaný stub konstruktoru
+	}
+
+	public ScenarioException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Automaticky generovaný stub konstruktoru
+	}
+
+	public ScenarioException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Automaticky generovaný stub konstruktoru
+	}
+
+}
--- org.perfclipse.core/src/org/perfclipse/scenario/ScenarioManager.java
+++ org.perfclipse.core/src/org/perfclipse/scenario/ScenarioManager.java
@@ -27,6 +27,7 @@ import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
+import org.slf4j.LoggerFactory;
 
 /**
  * ScenarioManger class provides methods for manipulating scenario.
@@ -37,15 +38,21 @@ public class ScenarioManager {
 
 	private Document scenario;
 	
+	final static org.slf4j.Logger log = LoggerFactory.getLogger(ScenarioManager.class);
 	/**
 	 * Load scenario from URL
 	 * @param scenarioUrl URL to scenario
-	 * @throws JDOMException
-	 * @throws IOException
+	 * @throws IOException 
+	 * @throws ScenarioException - when resource at given url cannot be parsed as XML scenario.
 	 */
-	public void load(URL scenarioUrl) throws JDOMException, IOException{
+	public void load(URL scenarioUrl) throws IOException, ScenarioException{
 		SAXBuilder builder = new SAXBuilder();
-		scenario = builder.build(scenarioUrl);
+		try {
+			scenario = builder.build(scenarioUrl);
+		} catch (JDOMException e) {
+			log.error("Cannot parse given file as XML scenario." + scenarioUrl.toString());
+			throw new ScenarioException("Cannot parse given file as XML scenario." + scenarioUrl.toString());
+		}
 	}
 	
 	/**
