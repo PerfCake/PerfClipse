c55356f2ed2c1ef0dfa15621f3313863d78b30c3
Jakub Knetl <knetl.j@gmail.com>
2013-11-27 15:57:57 +0100

    Run PerfCake scenario in new Thread.

--- org.perfclipse.ui/src/org/perfclipse/ui/handlers/RunHandler.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/handlers/RunHandler.java
@@ -29,7 +29,12 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
@@ -39,7 +44,6 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.IConsoleConstants;
 import org.eclipse.ui.console.IConsoleView;
 import org.eclipse.ui.console.MessageConsole;
-import org.eclipse.ui.console.MessageConsoleStream;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.perfclipse.scenario.ScenarioException;
 import org.perfclipse.scenario.ScenarioManager;
@@ -74,9 +78,6 @@ public class RunHandler extends AbstractHandler {
 	private void runScenario(IFile file, Shell shell) {
 		//redirect System.out to Eclipse console
 		MessageConsole perfclipseConsole = Utils.findConsole(Utils.PERFCLIPSE_STDOUT_CONSOLE);
-		OutputStream out = perfclipseConsole.newOutputStream();
-		PrintStream standardOut = System.out;
-		System.setOut(new PrintStream(out));
 		
 		//show console view
 		try{
@@ -94,26 +95,84 @@ public class RunHandler extends AbstractHandler {
 			LOGGER.warning("Cannot show console view since "
 					+ "getActiveWorkbenchWindow() is null.");
 		}
-	    
-	    //run scenario
-		ScenarioManager scenarioManager = new ScenarioManager();
 		
-		try {
-			scenarioManager.runScenario(file.getLocationURI().toURL());
-		} catch (ScenarioException e) {
-			LOGGER.warning("Cannot run scenario");
-			MessageDialog.openError(shell, "Scenario error", e.getMessage());
-		} catch (MalformedURLException e) {
-			LOGGER.warning("Wrong url to scenario.");
-			MessageDialog.openError(shell, "Scenario URL error", e.getMessage());
-		} finally {
-			try {
-				System.setOut(standardOut); //set System.out to standard output
-				out.close();
-			} catch (IOException e) {
-				LOGGER.warning("Cannot close stream to eclipse consolse!");
+		PerfCakeRunJob job = new PerfCakeRunJob("PerfCake run job", file, perfclipseConsole, shell);
+		job.schedule();
+	    
+	 
+	}
+	
+	final class PerfCakeRunJob extends Job{
+		
+		private IFile file;
+		private Shell shell;
+		private MessageConsole console;
+
+		public PerfCakeRunJob(String name, IFile file, MessageConsole console, Shell shell) {
+			super(name);
+			if (file == null){
+				LOGGER.warning("File with scenario is null.");
+				throw new IllegalArgumentException("File with scenario is null.");
 			}
+			if (console == null){
+				LOGGER.warning("Console for scenario run  output is null.");
+				throw new IllegalArgumentException("Console for scenario run output is null."); } if (shell == null){
+				LOGGER.warning("Parent shell is null.");
+				throw new IllegalArgumentException("Parent shell is null.");
+			}
+			this.file = file;
+			this.console = console;
+			this.shell = shell;
+			
 		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+		OutputStream out = console.newOutputStream();
+		PrintStream standardOut = System.out;
+		System.setOut(new PrintStream(out));
+				ScenarioManager scenarioManager = new ScenarioManager();
+				
+				try {
+					scenarioManager.runScenario(file.getLocationURI().toURL());
+				} catch (ScenarioException e) {
+					LOGGER.warning("Cannot run scenario");
+					Display.getDefault().asyncExec(new ErrorDialog(shell, "Scenario error", e.getMessage()));
+				} catch (MalformedURLException e) {
+					LOGGER.warning("Wrong url to scenario.");
+					Display.getDefault().asyncExec(new ErrorDialog(shell, "Scenario URL error", e.getMessage()));
+				} finally {
+					try {
+						System.setOut(standardOut); //set System.out to standard output
+						out.close();
+					} catch (IOException e) {
+						LOGGER.warning("Cannot close stream to eclipse consolse!");
+					}
+				}
+			return Status.OK_STATUS;
+		}
+		
+	}
+	
+	
+	final class ErrorDialog implements Runnable{
+		
+		private Shell parent;
+		private String title;
+		private String message;
+
+		public ErrorDialog(Shell parent, String title, String message){
+			this.parent = parent;
+			this.title = title;
+			this.message = message;
+		}
+
+		@Override
+		public void run() {
+			MessageDialog.openError(parent, title, message);
+			
+		}
+		
 	}
 
 }
