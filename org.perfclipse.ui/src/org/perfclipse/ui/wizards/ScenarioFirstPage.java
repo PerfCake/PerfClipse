8daec65eec68556eabb75d819311713d8f0f0a14
Jakub Knetl <knetl.j@gmail.com>
2014-02-04 23:00:51 +0100

    Create file and model on finishing scenario wizard

--- org.perfclipse.ui/src/org/perfclipse/ui/wizards/ScenarioFirstPage.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/wizards/ScenarioFirstPage.java
@@ -21,11 +21,16 @@ package org.perfclipse.ui.wizards;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -38,7 +43,9 @@ public class ScenarioFirstPage extends WizardPage {
 	private Combo senderCombo;
 	private Label generatorLabel;
 	private Combo generatorCombo;
-	private Button scenarioNameBrowseButton;
+	private Button scenarioDirectoryBrowseButton;
+	private Label scenarioDirectoryLabel;
+	private Text scenarioDirectoryText;
 
 	public ScenarioFirstPage(){
 		this("Scenario first page");
@@ -52,26 +59,84 @@ public class ScenarioFirstPage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
+		//TODO : gridData constructor with FILL_HORIZONTAL is deprecated
 		container = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 3;
+		layout.horizontalSpacing = 10;
+		layout.verticalSpacing = 10;
 		container.setLayout(layout);
 		
+		scenarioDirectoryLabel = new Label(container, SWT.NONE);
+		scenarioDirectoryLabel.setText("Choose scenario directory");
+
+		scenarioDirectoryText = new Text(container, SWT.NONE);
+		scenarioDirectoryText.setEditable(false);
+		scenarioDirectoryText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		
+		scenarioDirectoryBrowseButton = new Button(container, SWT.NONE);
+		scenarioDirectoryBrowseButton.setText("Browse");
+		scenarioDirectoryBrowseButton.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog dialog = new DirectoryDialog(getShell());
+				String result = dialog.open();
+				scenarioDirectoryText.setText(result);
+				setPageComplete(isPageComplete());
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});		
+		
+		final GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.horizontalSpan = 2;
+
 		scenarioNameLabel = new Label(container, SWT.NONE);
 		scenarioNameLabel.setText("Name of scenario: ");
 
 		scenarioNameText = new Text(container, SWT.NONE);
 		scenarioNameText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		scenarioNameText.setLayoutData(gridData);
+		scenarioNameText.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				setPageComplete(isPageComplete());
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
-		scenarioNameBrowseButton = new Button(container, SWT.NONE);
-		scenarioNameBrowseButton.setText("Browse");
 		
-		final GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
-		gridData.horizontalSpan = 2;
+
 		generatorLabel = new Label(container, SWT.NONE);
 		generatorLabel.setText("Choose generator");
 		generatorCombo = new Combo(container, SWT.NONE);
 		generatorCombo.add("DefaultMessageGenerator");
+		generatorCombo.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				setPageComplete(isPageComplete());
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		generatorCombo.setLayoutData(gridData);
 		
 
@@ -80,12 +145,66 @@ public class ScenarioFirstPage extends WizardPage {
 		
 		senderCombo = new Combo(container, SWT.NONE);
 		senderCombo.add("DummySender");
+		senderCombo.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				setPageComplete(isPageComplete());
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		senderCombo.setLayoutData(gridData);
 
+		
 		setControl(container);
-		setPageComplete(true);
+		setPageComplete(false);
 		
 		
+	}
+	
+	@Override
+	public boolean isPageComplete(){
+		if (scenarioDirectoryText == null || "".equals(scenarioDirectoryText.getText())){
+			setDescription("Fill in scenario directory location.");
+			return false;
+		}
+		if (scenarioNameText.getText() == null || "".equals(scenarioNameText.getText())){
+			setDescription("Fill in scenario name");
+			return false;
+		}
+		if (generatorCombo.getText() == null || "".equals(generatorCombo.getText())){
+			setDescription("Select generator type!");
+			return false;
+		}
+		if (senderCombo.getText() == null || "".equals(senderCombo.getText())){
+			setDescription("Select sender type!");
+			return false;
+		}
+		setDescription("Complete!");
+		
+		return true;
+	}
+	
+	public String getScenarioName(){
+		return scenarioNameText.getText();
+	}
+	
+	public String getScenarioDirectory(){
+		return scenarioDirectoryText.getText();
+	}
+	
+	public String getSenderName(){
+		return senderCombo.getText();
+	}
+	
+	public String getGeneratorName(){
+		return generatorCombo.getText();
 	}
 
 }
--- org.perfclipse.ui/src/org/perfclipse/ui/wizards/ScenarioWizard.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/wizards/ScenarioWizard.java
@@ -19,21 +19,36 @@
 
 package org.perfclipse.ui.wizards;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.perfcake.model.ObjectFactory;
+import org.perfcake.model.Scenario;
+import org.perfclipse.model.ScenarioModel;
+import org.perfclipse.scenario.ScenarioException;
+import org.perfclipse.scenario.ScenarioManager;
+import org.perfclipse.ui.editors.ScenarioDesignEditorInput;
 
 public class ScenarioWizard extends Wizard implements INewWizard {
 
-	private IWorkbench workbench;
 	private ScenarioFirstPage firstPage;
 	
-	private List<WizardPage> pages = new ArrayList<>();
 
 	public ScenarioWizard() {
 		super();
@@ -41,23 +56,70 @@ public class ScenarioWizard extends Wizard implements INewWizard {
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.workbench = workbench;
 
 	}
 
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
+		ObjectFactory scenarioFactory = new ObjectFactory();
+		Scenario scenario = scenarioFactory.createScenario();
+
+		//Generator section
+		Scenario.Generator generator = scenarioFactory.createScenarioGenerator();
+		Scenario.Generator.Run run = scenarioFactory.createScenarioGeneratorRun();
+		run.setType("time");
+		run.setValue("5000");
+		generator.setClazz(firstPage.getGeneratorName());
+		generator.setRun(run);
+		generator.setThreads("1");
+		scenario.setGenerator(generator);
+
+		//Sender section
+		Scenario.Sender sender = scenarioFactory.createScenarioSender();
+		sender.setClazz(firstPage.getSenderName());
+		scenario.setSender(sender);
+		
+		ScenarioModel model = new ScenarioModel(scenario);
+		
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		try {
+			URI uri = new URI("file://" + firstPage.getScenarioDirectory());
+			IContainer[] container = workspaceRoot.findContainersForLocationURI(uri);
+			if (container[0] instanceof IFolder){
+				IFolder folder = (IFolder) container[0];
+				IFile scenarioFile = folder.getFile(firstPage.getScenarioName());
+				
+				ScenarioManager manager = new ScenarioManager();
+				PipedOutputStream out = new PipedOutputStream();
+				PipedInputStream in = new PipedInputStream(out);
+				manager.createXML(model, out);
+				out.close();
+				scenarioFile.create(in, false, null);
+				
+				//open scenario editor
+				String scenarioEditorID = "org.perfclipse.ui.editors.scenario";
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				//TODO : editor cannot be initialized after Eclipse reload
+				page.openEditor(new ScenarioDesignEditorInput(scenarioFile), scenarioEditorID);
+				return true;
+			}
+		} catch (URISyntaxException e) {
+			MessageDialog.openError(getShell(), "URI syntax error", "Cannot locate selected folder for scenario.");
+		} catch (CoreException e) {
+			MessageDialog.openError(getShell(), "Core exception", "Cannot create or open file with scenario." );
+		} catch (IOException e) {
+			MessageDialog.openError(getShell(), "IO Exception", "Cannot create or read resource");
+		} catch (ScenarioException e) {
+			MessageDialog.openError(getShell(), "XML conversion error", "Cannot convert given data into XML.");
+		}
+		
 		return false;
 	}
 	
 	@Override
 	public void addPages(){
-		pages.add(new ScenarioFirstPage());
-
-		for(WizardPage p: pages){
-			addPage(p);
-		}
+		firstPage = new ScenarioFirstPage();
+		addPage(firstPage);
 	}
 
 }
