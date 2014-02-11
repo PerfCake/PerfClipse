788f2cbfb1618b88be849a15731e2cfe4534670b
Jakub Knetl <knetl.j@gmail.com>
2014-02-11 15:39:42 +0100

    Remove unnecessary methods

--- org.perfclipse.ui/src/org/perfclipse/ui/editors/ScenarioDesignEditorInput.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/editors/ScenarioDesignEditorInput.java
@@ -23,9 +23,7 @@ import java.net.MalformedURLException;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IPersistableElement;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
 import org.perfclipse.model.ScenarioModel;
@@ -37,43 +35,15 @@ public class ScenarioDesignEditorInput extends FileEditorInput {
 
 	final static org.slf4j.Logger log = LoggerFactory.getLogger(ScenarioDesignEditorInput.class);
 	private ScenarioModel model;
-
 	
 	public ScenarioDesignEditorInput(IFile file) {
 		super(file);
 		createModel();
 	}
-	
-
-
-	@Override
-	public boolean exists() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public ImageDescriptor getImageDescriptor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "Scenario Design editor";
-	}
-
-	@Override
-	public IPersistableElement getPersistable() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getToolTipText() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public ScenarioModel getModel() {
