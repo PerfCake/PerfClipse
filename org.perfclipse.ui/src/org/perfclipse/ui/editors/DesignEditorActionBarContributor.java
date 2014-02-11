93ef86366fccfeb8d4d41bacbb4dd1fe3fd22373
Jakub Knetl <knetl.j@gmail.com>
2014-02-11 15:55:35 +0100

    Add actionBarContributor
    * It allows redo and undo in Design Editor

---
+++ org.perfclipse.ui/src/org/perfclipse/ui/editors/DesignEditorActionBarContributor.java
@@ -0,0 +1,27 @@
+package org.perfclipse.ui.editors;
+
+import org.eclipse.gef.ui.actions.ActionBarContributor;
+import org.eclipse.gef.ui.actions.RedoRetargetAction;
+import org.eclipse.gef.ui.actions.UndoRetargetAction;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.ui.actions.ActionFactory;
+
+public class DesignEditorActionBarContributor extends ActionBarContributor {
+
+	@Override
+	protected void buildActions() {
+		addRetargetAction(new UndoRetargetAction());
+		addRetargetAction(new RedoRetargetAction());
+//		addRetargetAction(new DeleteRetargetAction());
+//		addRetargetAction(new LabelRetargetAction(ActionFactory.SELECT_ALL.getId(), "Select All"));
+	}
+	
+	@Override
+	public void contributeToToolBar(IToolBarManager toolBarManager) {
+		toolBarManager.add(getAction(ActionFactory.UNDO.getId()));
+		toolBarManager.add(getAction(ActionFactory.REDO.getId()));
+	}
+
+	protected void declareGlobalActionKeys() {
+	}
+}
