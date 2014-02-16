cef2560db2653e2a608f48277a33058c2d3af204
Jakub Knetl <knetl.j@gmail.com>
2014-02-16 11:20:34 +0100

    Refactor scanning method

--- org.perfclipse.core/src/org/perfclipse/reflect/ComponentScanner.java
+++ org.perfclipse.core/src/org/perfclipse/reflect/ComponentScanner.java
@@ -1,3 +1,22 @@
+/*
+ * PerfClispe
+ * 
+ *
+ * Copyright (c) 2014 Jakub Knetl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.perfclipse.reflect;
 
 import java.io.File;
@@ -19,6 +38,12 @@ import org.perfcake.validation.MessageValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Singleton class which is able to parse PerfCake bundle installed in eclipse
+ * and find implementation of components (Senders, Generators, ...)
+ * 
+ * @author Jakub Knetl
+ */
 public class ComponentScanner {
 	
 	final static Logger log = LoggerFactory.getLogger(ComponentScanner.class);
@@ -39,11 +64,11 @@ public class ComponentScanner {
 	private Set<Class<? extends MessageValidator>> validators;
 	
 	private ComponentScanner() throws PerfClipseScannerException{
-		senders = scanType(PERFCAKE_SENDER_PACKAGE, AbstractSender.class);
-		generators = scanType(PERFCAKE_GENERATOR_PACKAGE, AbstractMessageGenerator.class);
-		reporters = scanType(PERFCAKE_REPORTER_PACKAGE, Reporter.class);
-		destinations = scanType(PERFCAKE_DESTINATION_PACKAGE, Destination.class);
-		validators = scanType(PERFCAKE_VALIDATOR_PACKAGE, MessageValidator.class);
+		senders = scanForComponent(PERFCAKE_SENDER_PACKAGE, AbstractSender.class);
+		generators = scanForComponent(PERFCAKE_GENERATOR_PACKAGE, AbstractMessageGenerator.class);
+		reporters = scanForComponent(PERFCAKE_REPORTER_PACKAGE, Reporter.class);
+		destinations = scanForComponent(PERFCAKE_DESTINATION_PACKAGE, Destination.class);
+		validators = scanForComponent(PERFCAKE_VALIDATOR_PACKAGE, MessageValidator.class);
 		log.debug("Components included in PerfCake has been loaded.");
 	}
 	
@@ -79,7 +104,7 @@ public class ComponentScanner {
 	}
 
 	//TODO : remove duplicate information about type (both T and componentType)
-	private <T> Set<Class<? extends T>> scanType(String packageName, Class<T> componentType) throws PerfClipseScannerException{
+	private <T> Set<Class<? extends T>> scanForComponent(String packageName, Class<T> componentType) throws PerfClipseScannerException{
 		Set<Class<? extends T>> classes = new HashSet<>();
 		String packagePath = File.separator + packageName.replace(".", File.separator);
 		
@@ -100,22 +125,9 @@ public class ComponentScanner {
 				String[] files = packageDir.list();
 				for (String filename : files){
 					// TODO: if directory then recursion ???
-					if (filename.endsWith(".class")){
-						//trim .class extension
-						filename = filename.substring(0, filename.length() - 6);
-						try {
-							Class<?> clazz = Class.forName(packageName + "." + filename);
-							//if class is subclass of componentType
-							if (componentType.isAssignableFrom(clazz)){
-								// if class is not abstract
-								Class<? extends T> component = clazz.asSubclass(componentType);
-								if (!Modifier.isAbstract(clazz.getModifiers()))
-									classes.add(component);
-							}
-						} catch (ClassNotFoundException e) {
-							log.error("Cannot obtain class file for given file", e );
-							throw new PerfClipseScannerException("Cannot obtain class file for given file", e);
-						}
+					Class<? extends T> component = getClassOfType(packageName + "." + filename, componentType);
+					if (component != null){
+						classes.add(component);
 					}
 				}
 			}
@@ -125,6 +137,33 @@ public class ComponentScanner {
 		}
 		
 		return classes;
+	}
+	
+	private <T> Class<? extends T> getClassOfType(String path, Class<T> type) throws PerfClipseScannerException{
+		if (path.endsWith(".class")){
+			//trim .class extension
+			path= path.substring(0, path.length() - 6);
+			Class<?> clazz = getClassFromPackage(path);
+			//if class is subclass of componentType
+			if (type.isAssignableFrom(clazz)){
+				// if class is not abstract
+				Class<? extends T> component = clazz.asSubclass(type);
+				if (!Modifier.isAbstract(clazz.getModifiers()))
+					return component;
+			}
+		}
+		return null;
+		
+	}
+	
+	private Class<?> getClassFromPackage(String path) throws PerfClipseScannerException{
+		try {
+			Class<?> clazz = Class.forName(path);
+			return clazz;
+		} catch (ClassNotFoundException e) {
+			log.error("Cannot obtain class file for given file", e );
+			throw new PerfClipseScannerException("Cannot obtain class file for given file", e);
+		}
 	}
 
 }
