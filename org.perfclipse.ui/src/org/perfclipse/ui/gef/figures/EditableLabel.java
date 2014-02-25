c82beaff1ca5135564e33c83ca8aeee7c90455a3
Jakub Knetl <knetl.j@gmail.com>
2014-02-25 09:16:02 +0100

    Add editable label

---
+++ org.perfclipse.ui/src/org/perfclipse/ui/gef/figures/EditableLabel.java
@@ -0,0 +1,27 @@
+package org.perfclipse.ui.gef.figures;
+
+import org.eclipse.draw2d.Label;
+import org.eclipse.swt.graphics.Image;
+
+public class EditableLabel extends Label {
+
+	public EditableLabel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EditableLabel(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EditableLabel(Image i) {
+		super(i);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EditableLabel(String s, Image i) {
+		super(s, i);
+		// TODO Auto-generated constructor stub
+	}
+
+}
