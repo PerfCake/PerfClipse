88b0d0879afd2cafd11730f11f220cb818aa474b
Jakub Knetl <knetl.j@gmail.com>
2014-03-04 14:56:00 +0100

    Remove unused import

--- org.perfclipse.ui/src/org/perfclipse/ui/gef/parts/MessagesEditPart.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/gef/parts/MessagesEditPart.java
@@ -24,8 +24,6 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.StyledEditorKit.ForegroundAction;
-
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
