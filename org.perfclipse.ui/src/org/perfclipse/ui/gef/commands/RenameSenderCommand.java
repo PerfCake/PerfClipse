59a8c33774389ad5184719742db9ebfcedffd66b
Jakub Knetl <knetl.j@gmail.com>
2014-03-15 09:12:46 +0100

    Enable rename sender

---
+++ org.perfclipse.ui/src/org/perfclipse/ui/gef/commands/RenameSenderCommand.java
@@ -0,0 +1,55 @@
+/*
+ * Perfclispe
+ * 
+ * 
+ * Copyright (c) 2013 Jakub Knetl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.perfclipse.ui.gef.commands;
+
+import org.eclipse.gef.commands.Command;
+import org.perfclipse.model.SenderModel;
+
+/**
+ * @author Jakub Knetl
+ *
+ */
+public class RenameSenderCommand extends Command {
+	
+	private SenderModel sender;
+	private String oldClazz;
+	private String newClazz;
+	/**
+	 * @param sender
+	 * @param newClazz
+	 */
+	public RenameSenderCommand(SenderModel sender, String newClazz) {
+		super("Rename sender");
+		this.sender = sender;
+		this.newClazz = newClazz;
+		this.oldClazz = sender.getSender().getClazz();
+	}
+
+	@Override
+	public void execute() {
+		sender.setClass(newClazz);
+	}
+
+	@Override
+	public void undo() {
+		sender.setClass(oldClazz);
+	}
+	
+}
--- org.perfclipse.ui/src/org/perfclipse/ui/gef/parts/SenderEditPart.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/gef/parts/SenderEditPart.java
@@ -19,16 +19,50 @@
 
 package org.perfclipse.ui.gef.parts;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.RequestConstants;
+import org.eclipse.gef.tools.DirectEditManager;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ComboBoxViewerCellEditor;
+import org.eclipse.ui.PlatformUI;
 import org.perfclipse.model.SenderModel;
+import org.perfclipse.reflect.PerfCakeComponents;
+import org.perfclipse.reflect.PerfClipseScannerException;
+import org.perfclipse.ui.gef.directedit.ClassDirectEditManager;
+import org.perfclipse.ui.gef.directedit.ComboViewerCellEditorLocator;
+import org.perfclipse.ui.gef.figures.ILabeledFigure;
 import org.perfclipse.ui.gef.figures.TwoPartRectangle;
 import org.perfclipse.ui.gef.layout.colors.ColorUtils;
+import org.perfclipse.ui.gef.policies.directedit.SenderDirectEditPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SenderEditPart extends AbstractPerfCakeSectionEditPart {
+public class SenderEditPart extends AbstractPerfCakeSectionEditPart implements PropertyChangeListener {
 
+	static final Logger log = LoggerFactory.getLogger(SenderEditPart.class);
+
+	private DirectEditManager manager;
+	
+	@Override
+	public void activate() {
+		if (!isActive()){
+			getSenderModel().addPropertyChangeListener(this);
+		}
+		super.activate();
+	}
+
+	@Override
+	public void deactivate() {
+		getSenderModel().removePropertyChangeListener(this);
+		super.deactivate();
+	}
 	
 	public SenderEditPart(SenderModel senderModel){
 		setModel(senderModel);
@@ -52,9 +86,30 @@ public class SenderEditPart extends AbstractPerfCakeSectionEditPart {
 	}
 
 	@Override
+	public void performRequest(Request req) {
+		if (req.getType() == RequestConstants.REQ_OPEN ||
+				req.getType() == RequestConstants.REQ_DIRECT_EDIT){
+			PerfCakeComponents components;
+			try {
+				components = PerfCakeComponents.getInstance();
+				if (manager == null){
+					manager = new ClassDirectEditManager(this, ComboBoxViewerCellEditor.class,
+							new ComboViewerCellEditorLocator(((ILabeledFigure) getFigure()).getLabel()),
+									components.getSenders());
+				}
+				manager.show();
+			} catch (PerfClipseScannerException e) {
+				log.error("Cannot parse PerfCake components.", e);
+				MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+						"Cannot parse PerfCake components", "Edit is not possible");
+			}
+		}
+	}
+	
+	@Override
 	protected void createEditPolicies() {
-		// TODO Auto-generated method stub
-
+		installEditPolicy(EditPolicy.DIRECT_EDIT_ROLE,
+				new SenderDirectEditPolicy(getSenderModel(), (ILabeledFigure) getFigure()));
 	}
 	
 
@@ -62,6 +117,13 @@ public class SenderEditPart extends AbstractPerfCakeSectionEditPart {
 	protected List<Object> getModelChildren(){
 		List<Object> modelChildren = new ArrayList<Object>();
 		return modelChildren;
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals(SenderModel.PROPERTY_CLASS)){
+			refreshVisuals();
+		}
 	}
 
 }
