30f7c9c114528f0b1377650b1b7ab3c94fb16162
Jakub Knetl <knetl.j@gmail.com>
2014-03-30 10:34:36 +0200

    Change FIeldPreference page to normal preference page

--- org.perfclipse.ui/src/org/perfclipse/ui/preferences/ColorPreferencePage.java
+++ org.perfclipse.ui/src/org/perfclipse/ui/preferences/ColorPreferencePage.java
@@ -19,147 +19,199 @@
 
 package org.perfclipse.ui.preferences;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.preference.ColorFieldEditor;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.perfclipse.ui.Activator;
 
-public class ColorPreferencePage extends FieldEditorPreferencePage implements
+public class ColorPreferencePage extends PreferencePage implements
 		IWorkbenchPreferencePage {
 	
-	private ColorFieldEditor scenarioBg;
-	
-	private ColorFieldEditor generatorFg;
-	private ColorFieldEditor senderFg;
-	private ColorFieldEditor messagesFg;
-	private ColorFieldEditor messageFg;
-	private ColorFieldEditor validationFg;
-	private ColorFieldEditor validatorFg;
-	private ColorFieldEditor reportingFg;
-	private ColorFieldEditor reporterFg;
-	private ColorFieldEditor destinationFg;
-	private ColorFieldEditor propertiesFg;
-	private ColorFieldEditor propertyFg;
 
-	
-	private ColorFieldEditor generatorBg;
-	private ColorFieldEditor senderBg;
-	private ColorFieldEditor messagesBg;
-	private ColorFieldEditor messageBg;
-	private ColorFieldEditor validationBg;
-	private ColorFieldEditor validatorBg;
-	private ColorFieldEditor reportingBg;
-	private ColorFieldEditor reporterBg;
-	private ColorFieldEditor destinationBg;
-	private ColorFieldEditor propertiesBg;
-	private ColorFieldEditor propertyBg;
+	private List<FieldEditor> fieldEditors = new ArrayList<>();
 	
 	
 	public ColorPreferencePage(){
-		super(GRID);
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
 		setDescription("Set PerfCake components colors");
-		
-	}
-	@Override
-	public void init(IWorkbench workbench) {
-		
 	}
 	
-	
-
-
 	@Override
-	protected void createFieldEditors() {
+	protected Control createContents(Composite parent) {
+		Composite container = new Composite(parent, SWT.BORDER);
+
+		GridData data = new GridData();
+		data.grabExcessHorizontalSpace = true;
+		data.grabExcessVerticalSpace = true;
+		data.verticalAlignment = SWT.FILL;
+		data.horizontalAlignment = SWT.FILL;
+		parent.setLayoutData(data);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		container.setLayout(layout);
+		ColorFieldEditor scenarioBg;
+		
+
+		
 		scenarioBg = new ColorFieldEditor(PreferencesConstants.SCENARIO_COLOR_BACKGROUND,
-				"Scenario background: ", getFieldEditorParent());
+				"Editor background: ", container);
+
+		Group leftPane = new Group(container, SWT.NONE);
+		leftPane.setText("Components foreground: ");
+		data = new GridData(SWT.BEGINNING, SWT.FILL, false, true);
+		leftPane.setLayoutData(data);
+		Group rightPane = new Group(container, SWT.NONE);
+		data = new GridData(SWT.BEGINNING, SWT.FILL, false, true);
+		rightPane.setText("Components background: ");
+		rightPane.setLayoutData(data);
+
+		ColorFieldEditor generatorFg;
+		ColorFieldEditor senderFg;
+		ColorFieldEditor messagesFg;
+		ColorFieldEditor messageFg;
+		ColorFieldEditor validationFg;
+		ColorFieldEditor validatorFg;
+		ColorFieldEditor reportingFg;
+		ColorFieldEditor reporterFg;
+		ColorFieldEditor destinationFg;
+		ColorFieldEditor propertiesFg;
+		ColorFieldEditor propertyFg;
+
+		ColorFieldEditor generatorBg;
+		ColorFieldEditor senderBg;
+		ColorFieldEditor messagesBg;
+		ColorFieldEditor messageBg;
+		ColorFieldEditor validationBg;
+		ColorFieldEditor validatorBg;
+		ColorFieldEditor reportingBg;
+		ColorFieldEditor reporterBg;
+		ColorFieldEditor destinationBg;
+		ColorFieldEditor propertiesBg;
+		ColorFieldEditor propertyBg;
+
 		generatorFg = new ColorFieldEditor(PreferencesConstants.GENERATOR_COLOR_FOREGROUND,
-				"Generator foreground: ", getFieldEditorParent());
+				"Generator: ", leftPane);
 		generatorBg = new ColorFieldEditor(PreferencesConstants.GENERATOR_COLOR_BACKGROUND,
-				"Generator background", getFieldEditorParent());
+				"Generator", rightPane);
 
 		senderFg = new ColorFieldEditor(PreferencesConstants.SENDER_COLOR_FOREGROUND,
-				"Sender foreground: ", getFieldEditorParent());
+				"Sender: ", leftPane);
 		senderBg = new ColorFieldEditor(PreferencesConstants.SENDER_COLOR_BACKGROUND, "Sender background: ",
-				getFieldEditorParent());
+				rightPane);
 
 		messagesFg = new ColorFieldEditor(PreferencesConstants.MESSAGES_COLOR_FOREGROUND,
-				"Messages foreground: ", getFieldEditorParent());
+				"Messages: ", leftPane);
 		messagesBg = new ColorFieldEditor(PreferencesConstants.MESSAGES_COLOR_BACKGROUND,
-				"Messages background: ", getFieldEditorParent());
+				"Messages: ", rightPane);
 		messageFg = new ColorFieldEditor(PreferencesConstants.MESSAGE_COLOR_FOREGROUND,
-				"Message foreground: ", getFieldEditorParent());
+				"Message: ", leftPane);
 		messageBg = new ColorFieldEditor(PreferencesConstants.MESSAGE_COLOR_BACKGROUND,
-				"Message background: ", getFieldEditorParent());
+				"Message: ", rightPane);
 
 		validationFg = new ColorFieldEditor(PreferencesConstants.VALIDATION_COLOR_FOREGROUND,
-				"Validation foreground: ", getFieldEditorParent());
+				"Validation: ", leftPane);
 		validationBg = new ColorFieldEditor(PreferencesConstants.VALIDATION_COLOR_BACKGROUND,
-				"Validation background: ", getFieldEditorParent());
+				"Validation: ", rightPane);
 		validatorFg = new ColorFieldEditor(PreferencesConstants.VALIDATOR_COLOR_FOREGROUND,
-				"Validator foreground: ", getFieldEditorParent());
+				"Validator: ", leftPane);
 		validatorBg = new ColorFieldEditor(PreferencesConstants.VALIDATOR_COLOR_BACKGROUND,
-				"Validator background: ", getFieldEditorParent());
+				"Validator: ", rightPane);
 
 		reportingFg = new ColorFieldEditor(PreferencesConstants.REPORTING_COLOR_FOREGROUND,
-				"Reporting foreground: ", getFieldEditorParent());
+				"Reporting: ", leftPane);
 		reportingBg = new ColorFieldEditor(PreferencesConstants.REPORTING_COLOR_BACKGROUND,
-				"Reporting background: ", getFieldEditorParent());
+				"Reporting: ", rightPane);
 		reporterFg = new ColorFieldEditor(PreferencesConstants.REPORTER_COLOR_FOREGROUND,
-				"Reporter foreground: ", getFieldEditorParent());
+				"Reporter: ", leftPane);
 		reporterBg = new ColorFieldEditor(PreferencesConstants.REPORTER_COLOR_BACKGROUND,
-				"Reporter background: ", getFieldEditorParent());
+				"Reporter: ", rightPane);
 		destinationFg = new ColorFieldEditor(PreferencesConstants.DESTINATION_COLOR_FOREGROUND,
-				"Destination foreground: ", getFieldEditorParent());
+				"Destination: ", leftPane);
 		destinationBg = new ColorFieldEditor(PreferencesConstants.DESTINATION_COLOR_BACKGROUND,
-				"Destination background: ", getFieldEditorParent());
+				"Destination: ", rightPane);
 
 		propertiesFg = new ColorFieldEditor(PreferencesConstants.PROPERTIES_COLOR_FOREGROUND,
-				"Properties foreground: ", getFieldEditorParent());
+				"Properties: ", leftPane);
 		propertiesBg = new ColorFieldEditor(PreferencesConstants.PROPERTIES_COLOR_BACKGROUND,
-				"Properties background: ", getFieldEditorParent());
+				"Properties: ", rightPane);
 		propertyFg = new ColorFieldEditor(PreferencesConstants.PROPERTY_COLOR_FOREGROUND,
-				"Property foreground: ", getFieldEditorParent());
+				"Property: ", leftPane);
 		propertyBg = new ColorFieldEditor(PreferencesConstants.PROPERTY_COLOR_BACKGROUND,
-				"Property background: ", getFieldEditorParent());
+				"Property: ", rightPane);
 
-		addField(scenarioBg);
-
-		addField(generatorFg);
-		addField(generatorBg);
-
-		addField(senderFg);
-		addField(senderBg);
-
-		addField(messagesFg);
-		addField(messagesBg);
-		addField(messageFg);
-		addField(messageBg);
-
-		addField(validationFg);
-		addField(validationBg);
-		addField(validatorFg);
-		addField(validatorBg);
-		addField(destinationFg);
-		addField(destinationBg);
+		addFieldEditor(scenarioBg);
+		addFieldEditor(generatorFg);
+		addFieldEditor(senderFg);
+		addFieldEditor(messagesFg);
+		addFieldEditor(messageFg);
+		addFieldEditor(validationFg);
+		addFieldEditor(validatorFg);
+		addFieldEditor(reportingFg);
+		addFieldEditor(reporterFg);
+		addFieldEditor(destinationFg);
+		addFieldEditor(propertiesFg);
+		addFieldEditor(propertyFg);
+		addFieldEditor(generatorBg);
+		addFieldEditor(senderBg);
+		addFieldEditor(messagesBg);
+		addFieldEditor(messageBg);
+		addFieldEditor(validationBg);
+		addFieldEditor(validatorBg);
+		addFieldEditor(reportingBg);
+		addFieldEditor(reporterBg);
+		addFieldEditor(destinationBg);
+		addFieldEditor(propertiesBg);
+		addFieldEditor(propertyBg);
 		
-		addField(reportingFg);
-		addField(reportingBg);
-		addField(reporterFg);
-		addField(reporterBg);
-		addField(destinationFg);
-		addField(destinationBg);
+		for (FieldEditor e : fieldEditors){
+			initFieldEditor(e);
+		}
 
-		addField(propertiesFg);
-		addField(propertiesBg);
-		addField(propertyFg);
-		addField(propertyBg);
-
+		return container;
+	}
+	@Override
+	public boolean performCancel() {
+		return super.performCancel();
+	}
+	@Override
+	public boolean performOk() {
+		for (FieldEditor e : fieldEditors) {
+			e.store();
+		}
+		return super.performOk();
+	}
+	@Override
+	protected void performDefaults() {
+		for (FieldEditor e : fieldEditors){
+			e.loadDefault();
+		}
+		super.performDefaults();
 	}
 	
 	
+	private void addFieldEditor(FieldEditor editor){
+		fieldEditors.add(editor);
+	}
+	
+	private void initFieldEditor(FieldEditor editor){
+		editor.setPage(this);
+		editor.setPreferenceStore(getPreferenceStore());
+		editor.load();
+	}
 
-
-
+	@Override
+	public void init(IWorkbench workbench) {
+	}
 }
